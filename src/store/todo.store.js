997b219addd2b4340089bbd1033c79c634842cd3
hdz-kevin <kevinhdez592@gmail.com>
2025-02-01 11:28:05 -0600

    Define (without implementing) the initial functions to query and/or manipulate the state in a controlled manner

--- src/store/todo.store.js
+++ src/store/todo.store.js
@@ -17,7 +17,47 @@ const state = {
 
 const initStore = () => {
     console.log(state);
-    console.log("Itzel ♥");
 };
 
-export default { initStore };
+const loadStore = () => { throw new Error("Not implemented"); };
+
+const getTodos = (filter = Filters.All) => { throw new Error("Not implemented") };
+
+/**
+ * 
+ * @param {string} description Todo descriptions
+ */
+const addTodo = (description) => { throw new Error("Not implemented"); }
+
+/**
+ * 
+ * @param {string} todoId
+ */
+const toggleTodoStatus = (todoId) => { throw new Error("Not implemented"); }
+
+/**
+ * 
+ * @param {string} todoId
+ */
+const deleteTodo = (todoId) => { throw new Error("Not implemented"); }
+
+/**
+ * Delete all completed `todos` objects.
+ */
+const deletedCompleted = () => { throw new Error("Not implemented"); }
+
+const setFilter = (filter = Filters.All) => { throw new Error("Not implemented"); };
+
+const getCurrentFilter = () => { throw new Error("Not implemented"); };
+
+
+export default {
+    addTodo,
+    deletedCompleted,
+    deleteTodo,
+    getCurrentFilter,
+    initStore,
+    loadStore,
+    setFilter,
+    toggleTodoStatus,
+};
